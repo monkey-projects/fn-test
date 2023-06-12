e9327478a87590cbe56393cdc9067a54919f5945
Wout Neirynck <wout.neirynck@d-n.be>
2023-06-12 17:27:14 +0200

    Refactoring

--- src/main/clj/monkey/fn_test/http.clj
+++ src/main/clj/monkey/fn_test/http.clj
@@ -5,17 +5,34 @@
 
 (def request-line-pattern #"([A-Z]+)\s+(\S+)\s+HTTP/(\S+)")
 
-(defn- parse-request-line [line]
-  (if-let [[_ method path version] (re-matches request-line-pattern line)]
-    {:method (csk/->kebab-case-keyword method)
-     :path path
-     :version version}))
+(defn- parse-request-line [state lines]
+  (if-let [[_ method path version] (re-matches request-line-pattern (first lines))]
+    (assoc state
+           :result {:method (csk/->kebab-case-keyword method)
+                    :path path
+                    :version version}
+           :state :headers)))
 
-(defn- parse-header [state line]
-  (if (empty? line)
-    (assoc state :state :body)
-    (let [[k v] (clojure.string/split line #":")]
-      (assoc-in state [:result :headers (csk/->kebab-case-keyword k)] (.trim v)))))
+(defn- parse-header [state lines]
+  (if (empty? lines)
+    (assoc state :state :done)
+    (let [line (first lines)]
+      (if (empty? line)
+        (assoc state :state :body)
+        (let [[k v] (clojure.string/split line #":")]
+          (assoc-in state [:result :headers (csk/->kebab-case-keyword k)] (.trim v)))))))
+
+(defn- parse-body [state lines]
+  (-> state
+      (assoc :state :done)
+      (assoc-in [:result :body] lines)))
+
+(def initial-state {:state :initial})
+
+(def state-handlers
+  {:initial parse-request-line
+   :headers parse-header
+   :body parse-body})
 
 (defn parse-incoming
   "Parses incoming http request using very naive state machine."
@@ -23,17 +40,8 @@
   (let [ls (bs/to-line-seq buf)]
     (loop [state {:state :initial}
            lines ls]
-      (if-let [line (first lines)]
-        (let [{:keys [state] :as new-state}
-              (case (:state state)
-                :initial (assoc state
-                                :state :headers
-                                :result (parse-request-line line))
-                :headers (parse-header state line)
-                :body (-> state
-                          (assoc :state :done)
-                          (assoc-in [:result :body] lines)))]
-          (if (not= :done state)
-            (recur new-state (rest lines))
-            (:result new-state)))
-        (:result state)))))
+      (let [handler (get state-handlers (:state state))
+            {:keys [state] :as new-state} (handler state lines)]
+        (if (not= :done state)
+          (recur new-state (rest lines))
+          (:result new-state))))))
